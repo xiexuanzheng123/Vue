095e6f29dfcdc633b1d6688b10b73b12f4934cfa
xiexuanzheng123 <349328984@qq.com>
2017-03-05 14:22:49 +0800

    two demos of v-for

---
+++ js/v-for.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            {text: 'Learn Javascript'},
+            {text: 'Learn Vue.js'},
+            {text: 'Build Something Awesome'}
+        ]
+    }
+});
---
+++ js/v-forItems.js
@@ -0,0 +1,10 @@
+var example2 = new Vue({
+    el: '#example-2',
+    data: {
+        parentMessage: '菜鸟教程',
+        items: [
+            { message: 'www.runoob.com' },
+            { message: 'www.w3cschool.cc'}
+        ]
+    }
+});
